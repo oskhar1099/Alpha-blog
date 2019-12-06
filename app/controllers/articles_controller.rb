24a07059eee325b8a2ac5f66aa21cde74dc9504d
Rails Course <oskhara@uninorte.edu.co>
2019-12-06 11:52:53 -0500

    Add create, edit, update, show and listint

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,35 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
-    redirect_to_articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
